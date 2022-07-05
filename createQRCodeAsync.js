0d25394985f95f5dcc7932d1ad1a6c1bd5c9e529
julianlbs <julianlimalbs@gmail.com>
2022-07-04 23:40:21 -0300

    Second commit

--- createQRCodeAsync.js
+++ createQRCodeAsync.js
@@ -8,6 +8,18 @@ import process from "node:process";
 const start = Date.now();
 
 const numCPUs = cpus().length;
+const users = JSON.parse(fs.readFileSync("users.json").toString());
+
+async function createQRCode(users) {
+	const clusterUsers = users.filter(
+		(_, index) => index % numCPUs === cluster.worker.id - 1
+	);
+
+	for (const user of clusterUsers) {
+		await QRCode.toFile(`./qrcode-images/${user.id}.png`, user.cpf);
+		console.log(`${cluster.worker.id} salvou a imagem ${user.id}.png`);
+	}
+}
 
 if (cluster.isPrimary) {
 	console.log(`Number of CPUs is ${numCPUs}`);
@@ -17,25 +29,16 @@ if (cluster.isPrimary) {
 	for (let i = 0; i < numCPUs; i++) {
 		cluster.fork();
 	}
-
-	cluster.on("exit", (worker, code, signal) => {
-		console.log(`worker ${worker.process.pid} died`);
-		console.log("Let's fork another worker!");
-		cluster.fork();
-	});
 } else {
-	console.log(`Worker ${process.pid} started`);
+	console.log(`Worker ${cluster.worker.id} started`);
 
-	const users = JSON.parse(fs.readFileSync("users.json").toString());
-
-	async function createQRCode(users) {
-		await users.forEach((user) => {
-			QRCode.toFile(`./qrcode-images/${user.id}.png`, user.cpf);
+	createQRCode(users)
+		.then(() => process.exit(0))
+		.catch((err) => {
+			console.error(err);
+			process.exit(1);
 		});
-	}
-
-	createQRCode(users);
 }
 
 const end = Date.now();
-console.log(`Execution time: ${end - start} ms OR  ${(end - start) / 1000} s`);
+console.log(`Execution time: ${end - start} ms |  ${(end - start) / 1000} s`);
